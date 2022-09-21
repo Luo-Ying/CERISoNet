fab613e184906d9191f6a28625077d0e74cb83aa
Luo Yingqi <uapv1903453@pedago01c.univ-avignon.fr>
2022-09-21 23:55:46 +0200

    Initialiser projet CERISoNet

---
+++ monserver.js
@@ -0,0 +1,35 @@
+/**définit le middleware permettant de parser les données envoyées par la méthode post */
+const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+
+const https = require('https')
+const fs = require('fs')    // définit module fs (gestion des fichiers)
+
+const app = express();
+
+/**Charge le middleware bodyParser dans la pile pour lire les données au format HTML (&, =, %) */
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const options = {
+    key: fs.readFileSync('key.pem'),    // lecture clé privée et certificat
+    cert: fs.readFileSync('cert.pem')
+}
+
+/**lancement du serveur https avec clé et certificat associé */
+let server = https.createServer(options, app).listen(3231, () => {
+    console.log('HTTPS => listening on 3231')
+})
+
+/**Route racine('/') du server */
+app.get('/', (req, res) => {
+    console.log('listening on 3231')
+    res.sendFile(path.join(__dirname, './index.htm'))   // Renvoyer la fichier ./index.htm
+})
+
+/**Route '/login' */
+app.post('/login', (req, res) => {
+    console.log("username: ", req.body.username)
+    console.log("password: ", req.body.password)
+    res.send("Hello World!")
+})
